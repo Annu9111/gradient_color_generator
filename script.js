8db6563df14de2187c477e4ea032b2742c1de5f3
User <user@Users-MacBook-Pro.local>
2026-07-20 12:24:20 +0530

    javascript

---
+++ script.js
@@ -0,0 +1,5 @@
+let btn1 = document.getElementById("mybutton");
+let btn2 = document.getElementById("mybutton2");
+
+btn1.addEventListener("click",handleButton1);
+btn2.addEventListener("click",handleButton2);
