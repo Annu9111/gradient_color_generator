e6c40d77f4705b211474453a9fc16c2c44bb2252
User <user@Users-MacBook-Pro.local>
2026-07-20 13:33:25 +0530

    javascript

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let btn1 = document.getElementById("mybutton");
 let btn2 = document.getElementById("mybutton2");
 let copydiv = document.querySelector(".copy-code");
 
-let rgb1="#000";
-let rgb2 ="fff";
+let rgb1="rgb(92,145,229)";
+let rgb2 ="rgb(152,63,198)";
 const hexValue = ()=>{
     let myhexanum  = "0123456789abcdef";
     let colors = "#";
@@ -17,15 +17,21 @@ const hexValue = ()=>{
 
 const handleButton1 = ()=>{
     rgb1=hexValue();
+    btn1.innerText=rgb1;
     document.body.style.backgroundImage = `linear-gradient(to right,${rgb1},${rgb2})`
     copydiv.innerHTML=`linear-gradient(toright,${rgb1},${rgb2})`;
 };
 
 const handleButton2 = ()=>{
     rgb2=hexValue();
+    btn2.innerText =rgb2;
     document.body.style.backgroundImage = `linear-gradient(to right,${rgb1},${rgb2})`;
     copydiv.innerHTML=`linear-gradient(toright,${rgb1},${rgb2}))`;
 };
+
+copydiv.addEventListener("click",()=>{
+    navigator.clipboard.writeText(copydiv.innerText);
+})
 
 btn1.addEventListener("click",handleButton1);
 btn2.addEventListener("click",handleButton2);
